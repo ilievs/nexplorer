4fa4385ebd896e6221fa654dc14715a39dbcf956
ilievss <ilievss@abv.bg>
2020-08-28 14:03:15 +0300

    improve the Miller-Rabin tests

--- src/test/java/com/dreamlab/nexplorer/prime/impl/MillerRabinPrimalityTestTest.java
+++ src/test/java/com/dreamlab/nexplorer/prime/impl/MillerRabinPrimalityTestTest.java
@@ -4,13 +4,87 @@ import com.dreamlab.nexplorer.prime.PrimalityTest;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigInteger;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class MillerRabinPrimalityTestTest {
 
+    // Taken from https://primes.utm.edu/lists/2small/0bit.html
+    // Each number of bits n is mapped to a list of positive integers k, such that
+    // 2^n-k is a prime and is an n bit number
+    static Map<Integer, int[]> K_MAPPING = Map.of(
+            63, new int[]{25, 165, 259, 301, 375, 387, 391, 409, 457, 471},
+            100, new int[]{15, 99, 153, 183, 267, 285, 357, 479, 603, 833},
+            400, new int[]{593, 663, 767, 879, 1205, 2457, 3107, 3195, 3263, 3267});
+
+    static final List<BigInteger> _63_BIT_PRIMES = getPrimes(63);
+    static final List<BigInteger> _100_BIT_PRIME_NUMBERS = getPrimes(100);
+    static final List<BigInteger> _400_BIT_PRIME_NUMBERS = getPrimes(400);
+
+    private static final int MAX_TEST_NUMBERS = 5;
+
+    static final List<BigInteger> OVER_100_DIGIT_NON_PRIME_NUMBERS = Arrays.asList(
+            new BigInteger("3678617824563678216378678126378612783612783612786376231233523454353453453453453453268746237878326478"),
+            new BigInteger("1861782367826781637867812637861278361278423612786376231233523454353453453453453453268746237878326475"),
+            new BigInteger("8717236323678216786378678126378612783612783612786376231233523454353453453453453453268746237878326470"),
+            new BigInteger("7823647267821637869837812637861278361278361278637623123352345435345345345345345326874612436878326476"),
+        	new BigInteger("7823484274893216378671928126378612783612783612786376231233523454353453453453453453268746237878326472"),
+            new BigInteger("8971347823678216378678128726378612783612783612786376231233523454353453453453453453268746237878326474")
+    );
+
+    static final List<BigInteger> _1000_BIT_NON_PRIME_NUMBERS = getRandomNonPrimeNumbers(10, MAX_TEST_NUMBERS);
+    static final List<BigInteger> _4000_BIT_NON_PRIME_NUMBERS = getRandomNonPrimeNumbers(40, MAX_TEST_NUMBERS);
+
+    static final BigInteger _63_BIT_PRIME = BigInteger.valueOf(18_014_398_241_046_527L);
+
     private final PrimalityTest primalityTest = new MillerRabinPrimalityTest();
+
+    private static List<BigInteger> getPrimes(int nbits) {
+        if (!K_MAPPING.containsKey(nbits)) {
+            throw new IllegalArgumentException("nbits not supported");
+        }
+        int[] k = K_MAPPING.get(nbits);
+
+        BigInteger twoPowN = BigInteger.TWO.pow(nbits);
+        List<BigInteger> primes = new ArrayList<>();
+        for (int value : k) {
+            // we add one, because Long.MAX_VALUE = 2^63 - 1
+            // and we first subtract and then add, so we don't
+            // have an overflow
+            primes.add(twoPowN.subtract(BigInteger.valueOf(value)));
+        }
+
+        return primes;
+    }
+
+    // TODO generalize the method below even further for
+    //  generating random large non prime numbers
+    public static List<BigInteger> getRandomNonPrimeNumbers(int multiplesOf100Bits, int numbersToGenerate) {
+        if (numbersToGenerate < 1 || numbersToGenerate > 100) {
+            throw new IllegalArgumentException("numbersToGenerate has to be between 1 and 100");
+        }
+
+        if (multiplesOf100Bits < 1 || multiplesOf100Bits > 1000) {
+            throw new IllegalArgumentException("multiplesOf100Bits has to be between 1 and 1000");
+        }
+
+        Random random = new Random();
+        random.setSeed(System.currentTimeMillis());
+
+        List<BigInteger> randomNonPrimes = new ArrayList<>();
+        for (int i = 0; i < numbersToGenerate; i++) {
+            BigInteger num = BigInteger.ONE;
+            for (int j = 0; j < multiplesOf100Bits; j++) {
+                int randIdx = random.nextInt(_100_BIT_PRIME_NUMBERS.size());
+                num = num.multiply(_100_BIT_PRIME_NUMBERS.get(randIdx));
+            }
+            randomNonPrimes.add(num);
+        }
+
+        return randomNonPrimes;
+    }
 
     @Test
     public void testSmallPrimes() {
@@ -66,8 +140,9 @@ class MillerRabinPrimalityTestTest {
     }
 
     @Test
-    public void testVeryLargePrime() {
-        assertTrue(primalityTest.test(BigInteger.valueOf(18_014_398_241_046_527L)));
+    public void testVeryLargePrimes() {
+        assertTrue(primalityTest.test(_63_BIT_PRIME));
+        _63_BIT_PRIMES.forEach(p -> assertTrue(primalityTest.test(p)));
     }
 
     @Test
@@ -77,5 +152,30 @@ class MillerRabinPrimalityTestTest {
         assertFalse(primalityTest.test(BigInteger.valueOf(1073676280)));
         assertFalse(primalityTest.test(BigInteger.valueOf(18014398241046520L)));
         assertFalse(primalityTest.test(BigInteger.valueOf(1152780769266106369L)));
+    }
+
+    @Test
+    public void test100BitPrimes() {
+        _100_BIT_PRIME_NUMBERS.forEach(num -> assertTrue(primalityTest.test(num)));
+    }
+
+    @Test
+    public void test400BitPrimes() {
+        _400_BIT_PRIME_NUMBERS.forEach(num -> assertTrue(primalityTest.test(num)));
+    }
+
+    @Test
+    public void test100DigitNonPrimes() {
+        OVER_100_DIGIT_NON_PRIME_NUMBERS.forEach(num -> assertFalse(primalityTest.test(num)));
+    }
+
+    @Test
+    public void test1000BitNonPrimes() {
+        _1000_BIT_NON_PRIME_NUMBERS.forEach(num -> assertFalse(primalityTest.test(num)));
+    }
+
+    @Test
+    public void test4000BitNonPrimes() {
+        _4000_BIT_NON_PRIME_NUMBERS.forEach(num -> assertFalse(primalityTest.test(num)));
     }
 }
