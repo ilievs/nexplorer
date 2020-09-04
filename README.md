d981c9e3170616945673978540cd3490def7ac96
ilievss <ilievss@abv.bg>
2020-09-04 11:51:54 +0300

    add documentation and swagger ui for viewing the REST API docs

--- README.md
+++ README.md
@@ -1,3 +1,36 @@
 # nexplorer
-The 'n'  in "nexplorer" stands for "number". Nexplorer is a service
+The 'n' in "nexplorer" stands for "number". Nexplorer is a service
 that provides mathematical functions.
+
+## Getting started
+### Build
+
+Using Gradle:
+```bash
+./gradlew build
+```
+
+Optionally build a Docker image:
+
+```bash
+docker build -t nexplorer:0.0.1 .
+```
+
+### Run
+Run natively:
+
+```bash
+java -jar nexplorer-0.0.1.jar
+```
+
+or with Docker:
+
+```bash
+docker run -p 8080:8080 --name nexplorer nexplorer:0.0.1
+```
+
+### API documentation
+
+Access the API docs at:
+
+http://localhost:8080/swagger-ui/index.html
--- src/main/java/com/dreamlab/nexplorer/controller/FunctionController.java
+++ src/main/java/com/dreamlab/nexplorer/controller/FunctionController.java
@@ -3,6 +3,9 @@ package com.dreamlab.nexplorer.controller;
 import com.dreamlab.nexplorer.api.FunctionCallExecutor;
 import com.dreamlab.nexplorer.controller.model.FunctionCallPayload;
 import com.dreamlab.nexplorer.controller.model.FunctionCallResultPayload;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +20,17 @@ public class FunctionController {
     @Autowired
     private FunctionCallExecutor functionCallExecutor;
 
+    @ApiOperation(value = "Calls a mathematical function and returns the result of the execution")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    code = 200,
+                    message = "Returns the result of calling the function",
+                    response = FunctionCallResultPayload.class),
+
+            @ApiResponse(
+                    code = 400,
+                    message = "User supplied invalid input. Returns a message with a description of the user error")
+    })
     @PostMapping(value = "/call",
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
